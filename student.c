3828207259db0578fe817abbc76f6c9136f6ea6f
Sharadvi K J <sharadvikjgowda@gmail.com>
2026-02-15 23:05:44 +0530

    Student data

---
+++ student.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int id;
+    float m1,m2,m3,total,average;
+    prinf("Enter Student ID:");
+    scanf("%d",&id);
+    prinf("Enter Marks for 3 subjects:\n");
+    scanf("%f%f%f",&m1,&m2,&m3);
+    total=m1+m2+m3;
+    average=total/3;
+    printf("\n---Student Details---\n");
+    printf("Student ID:%d\n",id);
+    printf("Total Marks:%.2f\n",total);
+    printf("Average Marks:%.2f\n",average);
+    return 0;
+}
